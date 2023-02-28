9822fc0c263a05a40df980413d9b295161a7f15c
Izabel Cristina <izabelcrigo@gmail.com>
2023-02-27 23:16:45 -0300

    commit 5 implementação aplicação produtos implementação aplicação carrinho

--- core/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from core.models import Ecommerce
-class CarrinhoSerializer(ModelSerializer):
-    class Meta:
-        model= Ecommerce
-        fields ='produto', 'carrinho'
